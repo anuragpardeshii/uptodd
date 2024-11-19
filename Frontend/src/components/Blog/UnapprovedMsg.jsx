a2384a53a5954b77606a7b8880bdec109752bdb2
mounikavemulapalli <124512238+mounikavemulapalli@users.noreply.github.com>
2024-11-19 21:03:53 +0530

    Recommitting

--- Frontend/src/components/Blog/UnapprovedMsg.jsx
+++ Frontend/src/components/Blog/UnapprovedMsg.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./UnapprovedMsg.css"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./UnapprovedMsg.css";
 
 const UnapprovedMsg = () => {
   return (
     <div className='unapproved-message-wrapper'>
+      <div className='unapproved-message'>
+        <p>Sorry, replies to unapproved comments are not allowed</p>
+        <Link to='/blog'>«« Back</Link>
+      </div>
+    </div>
+  );
+};
 
-    <div className="unapproved-message">
-    <p>Sorry, replies to unapproved comments are not allowed</p>
-    <Link to="/blog">« Back</Link>
-
-  </div>
-  </div>
-
-  )
-}
-
-export default UnapprovedMsg
+export default UnapprovedMsg;
